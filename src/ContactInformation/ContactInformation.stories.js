1c75d8e238e529a1d8bd5ff1826d79aad5bd02b5
jaebradley <jae.b.bradley@gmail.com>
2018-02-09 10:38:50 -0500

    build(storybook): add contact information stories

--- src/ContactInformation/ContactInformation.stories.js
+++ src/ContactInformation/ContactInformation.stories.js
@@ -7,13 +7,32 @@ import Email from '../Email';
 import { SERVICE_TYPE, FONT_AWESOME_SIZE } from '../constants';
 
 storiesOf('ContactInformation', module)
-  .add('basic usage', () => (
-    <ContactInformation size={FONT_AWESOME_SIZE.TWO} rowLength={3}>
-      <Service username='jaebradley' type={SERVICE_TYPE.BITBUCKET} />
-      <Service username='jaebradley' type={SERVICE_TYPE.CODEPEN} />
+  .add('basic usage, single row', () => (
+    <ContactInformation size={FONT_AWESOME_SIZE.TWO} rowLength={6}>
+      <Service username='jaebradley' type={SERVICE_TYPE.ANGELLIST} />
       <Service username='jaebradley' type={SERVICE_TYPE.GITHUB}/>
       <Service username='jaebradley' type={SERVICE_TYPE.FACEBOOK} />
       <Service username='jaebradley' type={SERVICE_TYPE.TWITTER} />
+      <Service username='jaebradley' type={SERVICE_TYPE.CODEPEN} />
+      <Email to='jae.b.bradley@gmail.com' />
+    </ContactInformation>
+  ))
+  .add('multiple rows', () => (
+    <ContactInformation size={FONT_AWESOME_SIZE.TWO} rowLength={3}>
+      <Service username='jaebradley' type={SERVICE_TYPE.ANGELLIST} />
+      <Service username='jaebradley' type={SERVICE_TYPE.GITHUB}/>
+      <Service username='jaebradley' type={SERVICE_TYPE.FACEBOOK} />
+      <Service username='jaebradley' type={SERVICE_TYPE.TWITTER} />
+      <Service username='jaebradley' type={SERVICE_TYPE.CODEPEN} />
+      <Email to='jae.b.bradley@gmail.com' />
+    </ContactInformation>
+  ))
+  .add('font awesome size one', () => (
+    <ContactInformation size={FONT_AWESOME_SIZE.ONE}>
+      <Service username='jaebradley' type={SERVICE_TYPE.GITHUB}/>
+      <Service username='jaebradley' type={SERVICE_TYPE.FACEBOOK} />
+      <Service username='jaebradley' type={SERVICE_TYPE.TWITTER} />
+      <Service username='jaebradley' type={SERVICE_TYPE.CODEPEN} />
       <Email to='jae.b.bradley@gmail.com' />
     </ContactInformation>
   ));
